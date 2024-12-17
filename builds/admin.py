5305d0e38f29cde90ebbf47b39946064af4511a2
Naomi Dax <imkadax@gmail.com>
2024-12-17 13:36:10 +0000

    feat: Add Comment model

--- builds/admin.py
+++ builds/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BuildPost
+from .models import BuildPost, Comment
 
 # Register your models here.
 admin.site.register(BuildPost)
+admin.site.register(Comment)
--- builds/models.py
+++ builds/models.py
@@ -9,7 +9,7 @@ class BuildPost(models.Model):
     Modeel to store a single build post entry related to :model:`auth.User`.
     """
     build_title = models.CharField(max_length=150, unique=True)
-    author = models.ForeignKey(
+    build_author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="build_posts"
     )
     #image = CloudinaryField('image', default='placeholder')
@@ -17,15 +17,38 @@ class BuildPost(models.Model):
     #   max_length=100, default="",
     #   null=False, blank=False
     #)
-    description = models.TextField()
-    value = models.DecimalField(max_digits=10, decimal_places=2)
+    build_description = models.TextField()
+    money_spent = models.DecimalField(max_digits=8, decimal_places=2)
+    year_build = models.DateField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status_build_post = models.IntegerField(choices=STATUS, default=0)
+    #should be automated from the description field
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
-        
+
     def __str__(self):
-        return f"{self.title} | written by {self.author}"
+        return f"{self.build_title} | written by {self.build_author}"
+
+
+class Comment(models.Model):
+    """
+    Model to store a single comment entry related to :model:`auth.User`
+    and :model:`build_dream.BuildPost`. Needs to be approved
+    """
+    build_id = models.ForeignKey(BuildPost, on_delete=models.CASCADE,
+                             related_name="comments")
+    comment_author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    comment_title = models.CharField(max_length=100, unique=True)
+    comment_body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    comment_status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"Comment {self.comment_body} by {self.comment_author}"
