4d8616a4969a8524a4fcb7ecc74e838b5baecbd4
Naomi Dax <imkadax@gmail.com>
2024-12-18 10:39:54 +0000

    maint: Prepare code for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn build_your_dream.wsgi
--- build_your_dream/settings.py
+++ build_your_dream/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-xakkusu-p4buildyourdrea-zbw6jk1ztx3.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = ["8000-xakkusu-p4buildyourdrea-zbw6jk1ztx3.ws.codeinstitute-ide.net",".herokuapp.com"]
 
 
 # Application definition
