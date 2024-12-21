fd6b29b8e6f7742b2d26308a910d097050388a5e
Naomi Dax <imkadax@gmail.com>
2024-12-21 15:40:30 +0000

    feat: Add BuildPostClass to views.py

--- builds/views.py
+++ builds/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import BuildPost, Comment
 
 # Create your views here.
+class BuildPostList(generic.ListView):
+    """
+    Returns all published  build-posts and displays 8 posts 
+    per page paginated based on the date of creation.
+    """
+    model = BuildPost
+    queryset = BuildPost.objects.filter(status=1).order_by("created_on")
+    template_name = "build-your-dream/index.html"
+    paginate_by = 8
