2d01d36eeba80d3aa291918b802407b2c3142a30
Naomi Dax <imkadax@gmail.com>
2025-01-05 13:42:23 +0000

    maint: Create views.py with FutureRequestList class in future_content

--- future_content/views.py
+++ future_content/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import FutureContentRequest
 
 # Create your views here.
+
+
+class FutureRequestList(generic.ListView):
+    """
+    Returns all requests that will be implemented displays 4 requests 
+    per page paginated based on the date of creation.
+    """
+    #model = BuildPost
+    future_content = FutureContentRequest.objects.filter(status_build_post=2).order_by("created_on")
+    template_name = "future_content/future_content.html"
+    paginate_by = 4
+
+    
+
+def make_future_content_request(request):
+    """
+    User can make request to the Website owner about
+    future content that could be added
+    """
