a2976cd455a2033fa22e5a347752a3fcf40126b0
Naomi Dax <imkadax@gmail.com>
2025-01-05 12:11:47 +0000

    feat: Add basic show_build_post html to show singular posts

--- builds/urls.py
+++ builds/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.BuildPostList.as_view(), name='home'),
-    #path('<build_title:build_title>/', views.show_build_post, name='show_build_post'),
+    path('<slug:slug>/', views.show_build_post, name='show_build_post'),
 ]
--- builds/views.py
+++ builds/views.py
@@ -13,15 +13,15 @@ class BuildPostList(generic.ListView):
     template_name = "builds/index.html"
     paginate_by = 8
 
-#def show_build_post(request, build_title):
-#    """
-#    Displays/Shows singular build post with all it's content
-#    """
-#    queryset = BuildPost.objects.filter(status_build_post=2)
-#    build_post = get_object_or_404(queryset, build_title=build_title)
-#
-#   return render(
-#        request,
-#        "builds/show_build_post.html",
-#        {"build_post": build_post},
-#    )
+def show_build_post(request, slug):
+    """
+    Displays/Shows singular build post with all it's content
+    """
+    queryset = BuildPost.objects.filter(status_build_post=2)
+    build_post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "builds/show_build_post.html",
+        {"build_post": build_post},
+    )
