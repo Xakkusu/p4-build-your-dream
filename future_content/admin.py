ad9b8c9db51edb963b8528012966e15bbf99373a
Naomi Dax <imkadax@gmail.com>
2025-01-05 13:26:11 +0000

    maint: Add RequestAdmin class for admin panel and switch placements of URLs to access admin panel

--- future_content/admin.py
+++ future_content/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import FutureContentRequest
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(FutureContentRequest)
+class RequestAdmin(SummernoteModelAdmin):
+    list_display = ('future_content_title', 'future_content_author', 'status_of_request')
+    search_fields = ['future_content_title', 'status_of_request']
+    list_filter = ('status_of_request', 'created_on')
+    summernote_fields = ('future_content_description',)
+
+# Note: admin.ModelAdmin is the standard way of registering
+#       our model with the admin panel. We do it differently
+#       above because we are supplying Summernote fields.
+#       If you want to customise the admin panel view in your
+#       own projects, then inherit from admin.ModelAdmin like
+#       we do below.
