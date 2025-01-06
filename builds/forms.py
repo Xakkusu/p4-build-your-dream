f8d79f3bc5c7b1cf7683758e59e233924e52964a
Naomi Dax <imkadax@gmail.com>
2025-01-06 19:36:24 +0000

    maint: Add basic CreatBuildPost form-class to work with for the development stage

---
+++ builds/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import BuildPost
+
+class CreateBuildsPost(forms.ModelForm):
+    """
+    Class to create the form fields needed to create
+    a build post by the user from the views
+    """
+    class Meta:
+        """
+        Specify the model used and which fields will be
+        in the form
+        """
+        model = BuildPost
+        fields =["build_title ", "image_of_build", "image_alt", 
+                "build_description", "money_spent", "year_build"]
