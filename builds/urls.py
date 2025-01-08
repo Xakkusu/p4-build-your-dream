b581ed6694083c3cf8047a6c497d40e605c6b9dd
Naomi Dax <imkadax@gmail.com>
2025-01-08 19:55:09 +0000

    Install/Add crispy form, create and wire up basic create_build_post.html to see if form appears

--- builds/urls.py
+++ builds/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.BuildPostList.as_view(), name='home'),
+    path('createbuildpost/', views.CreateBuildPost.as_view(), name='create_build_post'),
     path('<slug:slug>/', views.show_build_post, name='show_build_post'),
 ]
