9d4bcb7c791d8bcf23ed1b5a2c441219b68b0a93
Naomi Dax <imkadax@gmail.com>
2025-01-12 10:18:20 +0000

    feat: Add page to edit and wire everything up correctly

--- builds/urls.py
+++ builds/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('createbuildpost/', views.CreateBuildPost.as_view(), name='create_build_post'),
     path('<slug:slug>/', views.show_build_post, name='show_build_post'),
     path('delete/<slug:pk>/', views.DeleteBuildPost.as_view(), name='delete_build_post'),
+    path('edit/<slug:pk>/', views.EditBuildPost.as_view(), name='edit_build_post'),
     path("<slug:slug>/edit_comment/<int:comment_id>/", views.edit_comment, name='edit_comment'),
     path("<slug:slug>/delete_comment/<int:comment_id>/", views.delete_comment, name='delete_comment'),
 ]
