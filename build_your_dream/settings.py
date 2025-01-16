ba935da49bae532c610137f21fe895bf420444f1
Naomi Dax <imkadax@gmail.com>
2025-01-16 18:24:49 +0000

    maint: Add cloudinary Url & file storage for deployment

--- build_your_dream/settings.py
+++ build_your_dream/settings.py
@@ -163,6 +163,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Cloudinary 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
