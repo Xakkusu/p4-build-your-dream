3c6dd16da730061487b22d2120116cd0ceea6998
Naomi Dax <imkadax@gmail.com>
2025-01-17 20:50:03 +0000

    fix: Correct Heroku error with env.py data

--- build_your_dream/settings.py
+++ build_your_dream/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-xakkusu-p4buildyourdrea-zbw6jk1ztx3.ws.codeinstitute-ide.net",".herokuapp.com"]
 
@@ -41,9 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
+    'cloudinary_storage',
     'django.contrib.sites',
 	'allauth',
 	'allauth.account',
@@ -52,6 +51,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'builds',
     'django_summernote',
+    'cloudinary',
     'future_content',
     'taggit',
     'rest_framework',
@@ -157,12 +157,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+CLOUDINARY = {
+      'CLOUDINARY_CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),  
+      'CLOUDINARY_API_KEY': os.environ.get("CLOUDINARY_API_KEY"),  
+      'CLOUDINARY_API_SECRET': os.environ.get("CLOUDINARY_API_SECRET"),
+}
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
